40a1427f19f0f258be349be45941894bf96fcae3
gayathri.ss <ssgayathri2001@gmail.com>
2023-09-04 17:47:34 +0530

    autofac added

---
+++ WEBAPI/Autofacmodule.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Autofac;
+using WEBAPI.Services;
+namespace WEBAPI
+
+{
+    public class Autofacmodule : Module
+    {
+        protected void Load(ContainerBuilder builder)
+        {
+            //Add transient
+            builder.RegisterType<crud>().As<icrud>()
+                .InstancePerDependency();
+            //builder.RegisterType<service>().As<Iservice>()
+            //   .SingleInstance();  Singleton
+
+            //    builder.RegisterType<service>().As<Iservice>()
+            //.InstancePerLifetimeScope(); Scoped
+        }
+    }
+}
--- WEBAPI/Program.cs
+++ WEBAPI/Program.cs
@@ -1,5 +1,10 @@
 using HangFireDemo.crud;
 using WEBAPI.Services;
+using Autofac;
+using WEBAPI;
+using Autofac.Extensions.DependencyInjection;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +17,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<icrud, crud>();
 
 var app = builder.Build();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new Autofacmodule());
+
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
